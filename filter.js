7d569d4a62be0a657766966be8e328b1d6ca2729
Elizabeth Clawson <emclawson1@gmail.com>
2024-09-02 11:25:56 -0500

    added to breedFilter event listener

--- filter.js
+++ filter.js
@@ -22,7 +22,23 @@ breedLoad()
 
 // create eventlistener for the breedFilter element
 breedFilter.addEventListener("change", (event)=> {
-    let index = breedFilter.selectedIndex;
-   
+    let index = breedFilter.selectedIndex; 
+    //getting the index of the cat breed within the array of cat breeds from breed load and caching in variable
+  
+   let selectedCatBreed = event.target[index];
+   //getting the information on that cat breed from breedLoad and caching in variable
 
+   let selectedBreedId = selectedCatBreed.id;
+   //getting the id of the catbreed and caching in variable to pass to breedInfo function 
+
+   breedInfo(selectedBreedId)
+   //passing the id to breedInfo
 })
+
+// function to grab the cat info and display on the page based on the selected cat id from the dropdown element
+async function breedInfo(id) {
+
+    const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${id}&api_key=live_Es3cWPLAwQH0X3Nk48kJtEgjzAskeBrzI2ZYlFrxkScKv8daA1DF4XJKiS704Sgx`);
+    console.log(response)
+
+}
