9ad207267c5e2f88c02f4a7298091b47c0175414
Elizabeth Clawson <emclawson1@gmail.com>
2024-09-02 23:35:24 -0500

    added favorites cards

--- favorites.js
+++ favorites.js
@@ -1,6 +1,7 @@
 //where POST request will be to add and delete favorites 
 
 //create a favorite 
+const favoriteCatContainer = document.getElementById("fave-cats");
 const buttons = document.getElementById("card-container")
 
 buttons.addEventListener("click", event =>{
@@ -26,19 +27,34 @@ async function clickedLike(id){
             body: rawBody,
         });
     console.log(response)   
+    displayFavorites();
 }
 
 // display a favorite 
 
-// async function displayFavorites() {
-//     const response = await fetch('https://api.thecatapi.com/v1/favourites?limit=20&sub_id=user-123&order=DESC', {
-//         headers: {
-//             "content-type":"application/json",
-//             "x-api-key" : "live_Es3cWPLAwQH0X3Nk48kJtEgjzAskeBrzI2ZYlFrxkScKv8daA1DF4XJKiS704Sgx",
-//         }
-//     });
-//     const favoriteCats = await response.json();
-// }
+async function displayFavorites() {
+    const response = await fetch('https://api.thecatapi.com/v1/favourites?limit=20&sub_id=user-123&order=DESC', {
+        headers: {
+            "content-type":"application/json",
+            "x-api-key": "live_Es3cWPLAwQH0X3Nk48kJtEgjzAskeBrzI2ZYlFrxkScKv8daA1DF4XJKiS704Sgx",
+        }
+    });
+    const favoriteCats = await response.json();
+    console.log(favoriteCats)
+    await favoriteCats.forEach(element => {
+       let id = element.image.id;
+       let url = element.image.url;
+       let faveCatCard = document.createElement("div");
+        faveCatCard.setAttribute("id", id);
+        faveCatCard.classList.add("card");
+        let cardImg = document.createElement("img");
+        cardImg.setAttribute("src",url)
+        cardImg.setAttribute("id", "cat-image")
+
+        favoriteCatContainer.appendChild(faveCatCard)
+        faveCatCard.appendChild(cardImg)
+    })
+}
 
 //delete favroites 
 
